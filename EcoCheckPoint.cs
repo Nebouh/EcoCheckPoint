47a971986dc85beb254b1ff19fb7db1cc7df8737
Nebouh <aureliendacruz45@hotmail.com>
2023-03-01 22:20:07 +0100

    Make the Save CheckPoint and Home Checkpoint work

--- EcoCheckPoint.cs
+++ EcoCheckPoint.cs
@@ -1,27 +1,43 @@
-using Eco.Gameplay.Players;
-using Eco.Gameplay.Systems.Messaging.Chat.Commands;
+﻿using Eco.Gameplay.Players;
 using Eco.Shared.Localization;
-using Eco.Shared.Math;
 using Eco.Shared.Utils;
-using System;
+using Eco.Gameplay.Systems.Messaging.Chat.Commands;
+using System.Numerics;
 
 namespace Eco.Mods.CheckPoint
 {
-	[ChatCommandHandler]
-	public class EcoCheckPointCommands
-	{
-		[ChatCommand("Lists of check points commands", "cp", ChatAuthorizationLevel.Admin)] public static void CheckPoint(User user) { }
+    [ChatCommandHandler]
+    public class EcoCheckPointCommands
+    {
+        public static Position position;
+        [ChatCommand("Lists of check points commands", "cp", ChatAuthorizationLevel.User)] public static void CheckPoint(User user) { }
 
-		[ChatSubCommand("CheckPoint", "Permit the player to save a location", "save", ChatAuthorizationLevel.Admin)]
-		public static void Wand(User user)
-		{
-			Log.WriteError(Localizer.Do($"Save location"));
-		}
-		
-		[ChatSubCommand("CheckPoint", "Permit the player to tp to last saved location", "tp", ChatAuthorizationLevel.Admin)]
-		public static void Wand(User user)
-		{
-			Log.WriteError(Localizer.Do($"tp location"));
-		}
-	}
+        [ChatSubCommand("CheckPoint", "Permit the player to save a location", "cps", ChatAuthorizationLevel.User)]
+        public static void CheckPointSave(User user)
+        {
+            Log.WriteError(Localizer.Do($"Save"));
+            position = Position.create("Test", user.Position);
+        }
+
+        [ChatSubCommand("CheckPoint", "Permit the player to tp to last saved location", "cph", ChatAuthorizationLevel.User)]
+        public static void CheckPointHome(User user)
+        {
+            Log.WriteError(Localizer.Do($"tp"));
+            user.Player.SetPosition(position.Localization);
+        }
+    }
+
+    public class Position
+    {
+        public string Name{ get; private set; }
+        public Vector3 Localization { get; private set; }
+
+        public static Position create(string positionName, System.Numerics.Vector3 localization)
+        {
+            Position position = new Position();
+            position.Name = positionName;
+            position.Localization = localization;
+            return position;
+        }
+    }
 }
